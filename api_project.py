bf0460ca819c3d4d01f2b3fc05c5b9b053a17d83
Aleksandra Kovaleva <alexsandrakovaleva@mail.ru>
2020-03-30 23:05:59 +0200

    So i changed the original idea of it being cuisine type to health. I haven't managed to make it run yet, printing out the "diet conscious recipes".

--- api_project.py
+++ api_project.py
@@ -2,8 +2,8 @@ import requests
 import os
 
 # Register to get an API key https://developer.edamam.com/edamam-recipe-api
-api_id = os.environ.get("edamam_api_id")
-api_key = os.environ.get("edamam_api_key")
+api_id = os.environ.get('edamam_api_id')
+api_key = os.environ.get('edamam_api_key')
 
 
 def recipe_search(mealType):
@@ -11,12 +11,24 @@ def recipe_search(mealType):
     data = result.json()
     return data['hits']
 
+def another_search(diet):
+    result1 = requests.get('https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(diet, api_id, api_key))
+    data1 = result1.json()
+    return data1['hits']
 
 def run():
-    mealType = input('Do you want breakfast, lunch, dinner or do you just want a snack? ')
+    mealType = input('Do you want breakfast, lunch, dinner or do you just want a snack?').lower()
 
-    if mealType in ["lunch", "Lunch", "dinner", "Dinner"]:
-        print("What type of cuisine do you fancy today?")
+    if mealType in ['lunch','dinner']:
+         health = input('Would you like your {} to be more diet aware i.e. low-carb, balanced?'.format(mealType))
+
+         if health == "yes":
+
+             recipes1 = recipe_search(diet)
+             for recipe in recipes1:
+                 print(recipe['recipe']['label'])
+                 print(recipe['recipe']['url'])
+                 print()
 
 
     else:
@@ -24,6 +36,7 @@ def run():
         for recipe in recipes:
             print(recipe['recipe']['label'])
             print(recipe['recipe']['url'])
+            print(recipe['recipe']['calories'])
             print()
 
 

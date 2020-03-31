dd9965d263af838725206e798de9344eefdb7490
vlnelson1 <62669980+vlnelson1@users.noreply.github.com>
2020-03-31 19:18:37 +0100

    Add files via upload
    needs tweaked (and one section doesn't work yet), but updated version with more code and some more details

--- api_project.py
+++ api_project.py
@@ -5,6 +5,10 @@ import os
 api_id = os.environ.get('edamam_api_id')
 api_key = os.environ.get('edamam_api_key')
 
+def recipe_search(ingredient):
+    result = requests.get('https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, api_id, api_key))
+    data = result.json()
+    return data['hits']
 
 def recipe_search(mealType):
     result = requests.get('https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(mealType, api_id, api_key))
@@ -16,27 +20,74 @@ def another_search(diet):
     info = output.json()
     return info['hits']
 
+def diet_check (health):
+    output = requests.get('https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(health, api_id, api_key))
+    info = output.json()
+    return info['hits']
+
+
 def run():
+    ingredient = input('Enter the key ingredient you want in your dish: ')
     mealType = input('Do you want breakfast, lunch, dinner or do you just want a snack?').lower()
 
     if mealType in ['lunch','dinner']:
          diet = input('Would you like your {} to be more diet aware i.e. low-carb, high-protein, balanced?'.format(mealType))
-
-
          if diet == 'yes':
-             recipes = another_search(diet) and recipe_search(mealType)
+             recipes = another_search(diet) and recipe_search(mealType) and recipe_search(ingredient)
              for recipe in recipes:
                  print(recipe['recipe']['label'])
                  print(recipe['recipe']['url'])
                  print(recipe['recipe']['calories'])
                  print()
+         else:
+            recipes_0=  recipe_search(mealType) and recipe_search(ingredient)
+            for recipe0 in recipes_0:
+                print(recipe0['recipe']['label'])
+                print(recipe0['recipe']['url'])
+                print(recipe0['recipe']['calories'])
+                print()
     else:
-        recipes1 = recipe_search(mealType)
-        for recipe in recipes1:
-            print(recipe['recipe']['label'])
-            print(recipe['recipe']['url'])
-            print(recipe['recipe']['calories'])
+        recipes1 = recipe_search(mealType) and recipe_search(ingredient)
+        for recipe1 in recipes1:
+            print(recipe1['recipe']['label'])
+            print(recipe1['recipe']['url'])
+            print(recipe1['recipe']['calories'])
             print()
+
+
+
+
+    serves = int(input('How many people are you serving?'))
+    print('Please see the recipes which suit your other needs along with their serving sizes')
+    recipes3 = recipe_search(mealType) and recipe_search(ingredient)
+    for recipe3 in recipes3:
+        serve = int(recipe3['recipe']['yield'])
+        if serve == serves:
+            print(recipe3['recipe']['label'])
+            print(recipe3['recipe']['url'])
+            print(recipe3['recipe']['calories'])
+            print(recipe3['recipe']['yield'])
+            print()
+        else:
+            print('No dishes were found to meet all criteria.')
+
+    sat = input('Are you satisfied with these selections?')
+    if sat == 'yes':
+        print("Enjoy your dish!")
+
+    else:
+        health = input('Do you need to know options which are more suitable for those with dietary restrictions?')
+        if health == 'yes':
+            recipes2 = recipe_search(mealType) and recipe_search(ingredient) and diet_check(health)
+            for recipe2 in recipes2:
+                print(recipe2['recipe']['label'])
+                print(recipe2['recipe']['url'])
+                print(recipe2['recipe']['calories'])
+                print()
+    print('End of program.')
+
+
+
 
 
 run()
